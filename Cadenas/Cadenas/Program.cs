4a0c9a89018d021712de03d64348f0b1eaf55e66
Dyffros <46058692+Dyffros@users.noreply.github.com>
2019-01-08 14:00:31 +0100

    Add files via upload

---
+++ Cadenas/Cadenas/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Cadenas
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string PrimerAmigo = "María";
+            string SegundoAmigo = "Sergio";
+            Console.WriteLine($"Mis amigos son {PrimerAmigo} y {SegundoAmigo}");
+
+            Console.WriteLine($"El nombre de {PrimerAmigo} tiene {PrimerAmigo.Length} letras");
+            Console.WriteLine($"El nombre de {SegundoAmigo} tiene {SegundoAmigo.Length} letras");
+            Console.ReadLine();
+        }
+    }
+}
